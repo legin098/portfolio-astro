8dd69410c7b4e22c39b81a4984790b37dd201db7
Jesus Cruz <jesus.cruz@campusvirtual.aunarvillavicencio.edu.co>
2023-01-17 17:46:16 -0500

    About components

---
+++ src/styles/colors.ts
@@ -0,0 +1,7 @@
+export const COLORS = {
+  WintergreenDream: "rgb(107, 144, 128)",
+  CambridgeBlue: "rgb(164, 195, 178)",
+  LightCyan: "rgb(204, 227, 222)",
+  AzureX11WebColor: "rgb(234, 244, 244)",
+  MintCream: "rgb(246, 255, 248)"
+};
